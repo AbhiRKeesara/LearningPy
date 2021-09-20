bd8c763794e3f41c15cf64fdb08582e31aa07e67
abhinay keesara <keesaraabhinay@gmail.com>
2021-09-19 16:57:45 -1000

    new file added to gitignore

--- recursion/palindromeChecker.py
+++ recursion/palindromeChecker.py
@@ -14,18 +14,23 @@ def palindrome_checker_one(string: str) -> bool:
             return True
         else:
             if s[start] == s[end]:
-                return palindrome_helper(s, start + 1, end + 1)
+                return palindrome_helper(s, start + 1, end - 1)
             else:
                 return False
 
     return palindrome_helper(string, 0, len(string) - 1)
 
 
+def remove_white(s):
+    return ''.join(e for e in s if e.isalnum())
+
 if __name__ == "__main__":
     str1: str = "kayak"
     str2: str = "aibohphobia"
-
-    print(palindrome_checker(str1))
-    print(palindrome_checker(str2))
-    print(palindrome_checker_one(str1))
-    print(palindrome_checker_one(str2))
+    str3: str = "madam i'm adam"
+    print(palindrome_checker(remove_white(str1)))
+    print(palindrome_checker(remove_white(str2)))
+    print(palindrome_checker(remove_white(str3)))
+    print(palindrome_checker_one(remove_white(str1)))
+    print(palindrome_checker_one(remove_white(str2)))
+    print(palindrome_checker_one(remove_white(str3)))
